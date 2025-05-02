b3558f6a39ee137f86bb575e33bab108c68dec33
Andria Molashvili <117169069+AndriaMolashvili@users.noreply.github.com>
2025-05-02 11:34:36 -0700

    Add files via upload

--- Level 27/Homework/lesson.py
+++ Level 27/Homework/lesson.py
@@ -26,22 +26,28 @@
 
 # -------------------------------------------------------------------------------------------------
 
-txt = input('შეიყვანეთ ტექსტი: ')
-txt = txt.upper()
-print(txt)
+# txt = input('შეიყვანეთ ტექსტი: ')
+# txt = txt.upper()
+# print(txt)
 
-txt2 = input('შეიყვანეთ მეორე ტექსტი: ')
-txt2 = txt2.lower()
-print(txt2)
+# txt2 = input('შეიყვანეთ მეორე ტექსტი: ')
+# txt2 = txt2.lower()
+# print(txt2)
 
-txt3 = input('შეიყვანეთ მესამე ტექსტი: ')
-txt3 = txt3.capitalize()
-print(txt3)
+# txt3 = input('შეიყვანეთ მესამე ტექსტი: ')
+# txt3 = txt3.capitalize()
+# print(txt3)
 
-txt4 = input('შეიყვანეთ მეოთხე ტექსტი: ')
-txt4 = txt4.swapcase()
-print(txt4)
+# txt4 = input('შეიყვანეთ მეოთხე ტექსტი: ')
+# txt4 = txt4.swapcase()
+# print(txt4)
 
-txt5 = input('შეიყვანეთ მეხუთე ტექსტი: ')
-txt5 = txt5.find('tbilisi')
-print(txt5)
+# txt5 = input('შეიყვანეთ მეხუთე ტექსტი: ')
+# txt5 = txt5.find('tbilisi')
+# print(txt5)
+
+# -------------------------------------------------------------------------------------------------
+
+def birthday(month, day):
+    print(f'Your birthday is on {day} of {month}')
+birthday('April', 18)
